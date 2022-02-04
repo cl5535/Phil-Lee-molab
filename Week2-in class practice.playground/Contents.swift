c6a11940779747db31dd958df6ab9eed3c8ee510
cl5535 <95434630+cl5535@users.noreply.github.com>
2022-02-04 17:33:26 -0500

    week 2 in class practice

---
+++ Week2-in class practice.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+let x = 150
+
+print("x = \(x)")
+//or
+print("x =", x)
+
+
+//---------------------
+
+let str = "ABCDEFG"
+
+print("str =", str)
+print(str.count)
+print(str[str.startIndex])
+print(str[str.index(str.startIndex, offsetBy:3)])
+
+//---------------------
+
+import Foundation
+
+let nstr = "Mozilla"
+
+
