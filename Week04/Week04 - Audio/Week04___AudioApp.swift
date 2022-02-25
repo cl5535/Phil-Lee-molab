456166839b1b549de79507084840b51993fd2aca
Phil Lee <95434630+cl5535@users.noreply.github.com>
2022-02-25 00:03:36 -0500

    Add files via upload

---
+++ Week04/Week04 - Audio/Week04___AudioApp.swift
@@ -0,0 +1,16 @@
+//___FILEHEADER___
+
+import SwiftUI
+
+@main
+struct AudioDemoApp: App {
+  
+//  @StateObject var audioDJ = AudioDJ()
+  
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+//        .environmentObject(audioDJ)
+    }
+  }
+}
