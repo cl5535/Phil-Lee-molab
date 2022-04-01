639fb48317abf86cfe92a823980ba0333e5a65d3
cl5535 <95434630+cl5535@users.noreply.github.com>
2022-04-01 11:51:25 -0400

    update

--- Audio/Audio/AudioViewModel.swift
+++ Audio/Audio/AudioViewModel.swift
@@ -24,4 +24,8 @@ struct Audio {
     let image: String
     
     static let data = Audio(title: "Won't Go Home Without You", description: "Album: It Won't Be Soon Before Long", description2: "Artist: Maroon 5", duration: 232, track: "Maroon 5 - Won't Go Home Without You", image: "Maroon 5 - Won't be soon before long2")
+    
+    static let data2 = Audio(title: "Beautiful Mistakes ft. Megan Thee Stallion", description: "Album: Red Pill Blues", description2: "Artist: Maroon 5", duration: 229, track: "Maroon 5 - Beautiful Mistakes ft. Megan Thee Stallion", image: "Marron 5 - red pill blues")
+    
+    static let data3 = Audio(title: "Girls Like You ft. Cardi B", description: "Album: Jordi", description2: "Artist: Maroon 5", duration: 271, track: "Maroon 5 - Girls Like You ft. Cardi B", image: "Maroon 5 - Jordi")
 }
--- Audio/Audio/ContentView.swift
+++ Audio/Audio/ContentView.swift
@@ -7,10 +7,61 @@
 
 import SwiftUI
 
+//struct ContentView: View {
+//    var body: some View {
+//        AudioPage(audioVM: AudioViewModel(audio: Audio.data))
+//    }
+//}
+
 struct ContentView: View {
     var body: some View {
-        AudioPage(audioVM: AudioViewModel(audio: Audio.data))
+        NavigationView {
+            ZStack {
+                Color(red: 24/255, green: 23/255, blue: 22/255, opacity: 100)
+            VStack(alignment: .leading, spacing: 50) {
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Favorite Maroon5 Music")
+                        .fontWeight(.black)
+                        .foregroundColor(Color.white)
+                        .font(.title)
+                        .padding(.bottom)
+                    
+                    
+                NavigationLink (destination: AudioPage(audioVM: AudioViewModel(audio: Audio.data))) {
+                    Text("Won't Go Home Withought You")
+                        .font(.title3)
+                        .fontWeight(.medium)
+                        .foregroundColor(Color.gray)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                }
+                
+                
+                NavigationLink (destination: AudioPage(audioVM: AudioViewModel(audio: Audio.data2))) {
+                    Text("Beautiful Mistakes")
+                        .font(.title3)
+                        .fontWeight(.medium)
+                        .foregroundColor(Color.gray)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                }
+                    
+                    NavigationLink (destination: AudioPage(audioVM: AudioViewModel(audio: Audio.data3))) {
+                        Text("Girls Like You")
+                            .font(.title3)
+                            .fontWeight(.medium)
+                            .foregroundColor(Color.gray)
+                            .multilineTextAlignment(.center)
+                            .padding()
+                    }
+                    
     }
+            }
+        }
+            .frame(height: UIScreen.main.bounds.height)
+            .ignoresSafeArea()
+    }
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
